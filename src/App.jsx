ee00a4595cfa811cf7eaf03524d3e9ff7c8fa198
R S Rashmika <srashmika144@gmail.com>
2023-04-03 12:35:09 +0530

    nav dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import { BrowserRouter } from "react-router-dom";
 
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Identification from "./components/Identification";
+import Learn from "./components/Learn";
+import Services from "./components/Services";
+import Treatments from "./components/Treatments";
+
 const App = () => {
-	return <BrowserRouter></BrowserRouter>;
+	return (
+		<BrowserRouter>
+			<div className="relative z-0 bg-primary">
+				<div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+					<Navbar />
+					<Hero />
+				</div>
+				<Services />
+				<Treatments />
+				<Learn />
+				<div className="relative z-0">
+					<Identification />
+				</div>
+			</div>
+		</BrowserRouter>
+	);
 };
 
 export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,19 @@
+export const navLinks = [
+    {
+        id: "services",
+        title: "Services",
+    },
+    {
+        id: "treatments",
+        title: "Treatments",
+    },
+    {
+        id: "learn",
+        title: "Learn",
+    },
+    {
+        id: "identification",
+        title: "Identification",
+    }
+];
+
