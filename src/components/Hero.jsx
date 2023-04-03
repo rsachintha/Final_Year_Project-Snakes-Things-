d3668945e76bb89b090c6d56418369cdde1589e1
R S Rashmika <srashmika144@gmail.com>
2023-04-03 15:25:19 +0530

    animation #1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import SnakeCanvas from "./canvas/Snake1";
 
 const Hero = () => {
-	return <div>Hero</div>;
+	return (
+		<section className="relative w-full h-screen mx-auto">
+			<div
+				className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+			>
+				<div className="flex flex-col justify-center items-center mt-5"></div>
+				<div>
+					<h1 className={`${styles.heroHeadText} text-white`}>
+						Welcome, to{" "}
+						<span className="text-[#915eff]">The Reptiles World</span>
+					</h1>
+					<p className={`${styles.heroSubText} mt-2 text-white-100`}>
+						Explore, identify and get educated regarding snake species around
+						the world with us.
+					</p>
+				</div>
+			</div>
+			<SnakeCanvas />
+		</section>
+	);
 };
 
 export default Hero;
---
+++ src/components/canvas/Snake2.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Snake2 = () => {
+	return <div>Snake2</div>;
+};
+
+export default Snake2;
