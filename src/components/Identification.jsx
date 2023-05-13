468680680b24818310622dbf3c446891d15fffda
R S Rashmika <srashmika144@gmail.com>
2023-05-13 23:06:37 +0530

    modifications #1

--- src/components/Identification.jsx
+++ src/components/Identification.jsx
@@ -5,104 +5,119 @@ import { styles } from "../styles";
 import CanvasTwo from "./canvas/Snake2";
 import SectionWrapper from "../hoc/SectionWrapper";
 import { slideIn } from "../utils/motion";
+import Modal from "./Modal";
+import axios from "axios";
 
 const Identification = () => {
-	// const [loading, setLoading] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
+	const [disable, setDisable] = useState(false);
+	const [file, setFile] = useState(null);
 
-	// const handleSubmit = (e) => {
-	// 	e.preventDefault();
-	// 	setLoading(true);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setIsLoading(true);
 
-	// 	emailjs
-	// 		.send(
-	// 			import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-	// 			import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-	// 			{
-	// 				from_name: form.name,
-	// 				to_name: "JavaScript Mastery",
-	// 				from_email: form.email,
-	// 				to_email: "sujata@jsmastery.pro",
-	// 				message: form.message,
-	// 			},
-	// 			import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-	// 		)
-	// 		.then(
-	// 			() => {
-	// 				setLoading(false);
-	// 				alert("Thank you. I will get back to you as soon as possible.");
+		const data = new FormData();
+		data.append("imagefile", file);
+		const config = {
+			headers: {
+				"content-type": "multipart/form-data",
+			},
+		};
+		const url = "http://localhost:3000/predict";
+		axios
+			.post(url, data, config)
+			.then((response) => {
+				console.log(response.data);
+				setIsOpen(true);
+			})
+			.catch((err) => {
+				console.log("err", err);
+			});
+	};
 
-	// 				setForm({
-	// 					name: "",
-	// 					email: "",
-	// 					message: "",
-	// 				});
-	// 			},
-	// 			(error) => {
-	// 				setLoading(false);
-	// 				console.error(error);
-
-	// 				alert("Ahh, something went wrong. Please try again.");
-	// 			}
-	// 		);
-	// };
+	const handleChange = (e) => {
+		setFile(e.target.files[0]);
+		setDisable(true);
+		// console.log(file);
+	};
 
 	return (
-		<div
-			className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
-		>
-			<motion.div
-				variants={slideIn("left", "tween", 0.2, 1)}
-				className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
-			>
-				<h3 className={styles.sectionHeadText}>Let's Identify It Here</h3>
-
-				<form
-					// ref={formRef}
-					// onSubmit={handleSubmit}
-					className="mt-12 flex flex-col gap-8"
-				>
-					{/* <label className="flex flex-col">
-						<span className="text-white font-medium mb-4">Your Name</span>
-						<input
-							type="text"
-							name="name"
-							value={form.name}
-							onChange={handleChange}
-							placeholder="What's your good name?"
-							className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
-						/>
-					</label>
-					<label className="flex flex-col">
-						<span className="text-white font-medium mb-4">Your email</span>
-						<input
-							type="email"
-							name="email"
-							value={form.email}
-							onChange={handleChange}
-							placeholder="What's your web address?"
-							className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
-						/>
-					</label> */}
-					<label className="flex flex-col">
-						<span className="text-white font-medium mb-4">Snake's Image</span>
-					</label>
-
-					<button
-						type="submit"
-						className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
+		<>
+			{isOpen ? (
+				<Modal setIsOpen={setIsOpen}></Modal>
+			) : (
+				<>
+					<div
+						className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
 					>
-						{/* {loading ? "Identifying..." : "Done!"} */}
-					</button>
-				</form>
-			</motion.div>
+						<motion.div
+							variants={slideIn("left", "tween", 0.2, 1)}
+							className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+						>
+							<h3 className={styles.sectionHeadText}>Let's Identify It Here</h3>
 
-			<motion.div
-				variants={slideIn("right", "tween", 0.2, 1)}
-				className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
-			>
-				<CanvasTwo />
-			</motion.div>
-		</div>
+							<form
+								className="mt-12 flex flex-col gap-8"
+								// encType="multipart/form-data"
+								onSubmit={handleSubmit}
+							>
+								{disable ? (
+									<label className="flex flex-col rounded-xl uppercase bg-tertiary text-center px-10 py-3 shadow-md shadow-primary">
+										<span className="text-white font-medium">
+											File Uploaded
+										</span>
+										<input
+											disabled
+											name="imagefile"
+											className="hidden"
+											// className="shadow-md shadow-primary bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+											type="file"
+											onChange={handleChange}
+										/>
+									</label>
+								) : (
+									<label className="flex flex-col rounded-xl uppercase green-pink-gradient text-center px-10 py-3 shadow-md shadow-primary cursor-pointer">
+										<span className="text-white font-medium">Upload File</span>
+										<input
+											name="imagefile"
+											className="hidden"
+											// className="shadow-md shadow-primary bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+											type="file"
+											onChange={handleChange}
+										/>
+									</label>
+								)}
+
+								{isLoading ? (
+									<button
+										// type="submit"
+										className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
+									>
+										Predicting...
+									</button>
+								) : (
+									<button
+										type="submit"
+										className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
+									>
+										Click to Predict
+									</button>
+								)}
+							</form>
+						</motion.div>
+
+						<motion.div
+							variants={slideIn("right", "tween", 0.2, 1)}
+							className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+						>
+							<CanvasTwo />
+						</motion.div>
+					</div>
+				</>
+			)}
+		</>
 	);
 };
 
---
+++ src/components/Modal.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import image from "../assets/modal.png";
+
+function Modal(props) {
+	return (
+		// <div className="w-screen h-screen bg-primary fixed flex justify-center items-center">
+		<div className="w-[500px] h-[500px] rounded-xl bg-secondary shadow-md flex flex-col p-6 ml-[330px]">
+			<div className="flex justify-end">
+				<button
+					className="bg-transparent border-none text-2xl cursor-pointer text-primary font-bold"
+					onClick={() => {
+						props.setIsOpen(false);
+					}}
+				>
+					X
+				</button>
+			</div>
+			<div className="block text-center text-3xl mt-3 font-semibold">
+				<h1>Predicted Snake Species Is,</h1>
+				<img
+					src={image}
+					alt="modal_img"
+					className="object-cover opacity-80 tra"
+				/>
+			</div>
+			<div className=" flex-[50%] flex justify-center items-center text-center text-2xl font-bold text-primary">
+				<p>Black Mamba</p>
+			</div>
+		</div>
+		// </div>
+	);
+}
+
+export default Modal;
